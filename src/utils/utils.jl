6ebcedaad6b0eaa12b539feb7eb25a1495f67ef3
Johnny Chen <johnnychen94@hotmail.com>
2020-08-11 23:40:15 +0800

    add `by` keyword to `with_cache_dir` and make the logic clearer
    Now `with_cache_dir` will clean up cache dir in either of the following cases:
    
    * `f() == false` or throws an exception
    * `by(cache_dir) == false`

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -33,18 +33,69 @@ function timeout_call(f::Function, timeout::Real; pollint=0.1)
 end
 
 """
-    with_cache_dir(f, cache_dir)
+    with_cache_dir(f, cache_dir; by=isdir) -> Bool
 
-If `cache_dir` already exists, then skip running `f()`.
+Conditionally run function `f()` based on the status of `by(cache_dir)`.
 
-`f` should return `true` to indicate a success status. In any other cases, `cache_dir` will be 
-cleaned up.
+If `by(cache_dir) == false`, then `f()` get called. By default, `by = isdir` and thus doesn't verify
+the contents of `cache_dir`.
+
+`cache_dir` will be cleaned up if either of the case happens:
+
+* an exception is thrown,
+* `f() == false`
+* `by(cache_dir) == false` 
+
+
+!!! note
+    The return value indicates only whether the `cache_dir` is kept. A `true` return valuet doesn't
+    necessarily indicate that `f()` gets called successfully; `f()` might never get called.
+
+
+# Examples
+
+```jldoctest
+julia> cache_dir = tempname();
+
+julia> logfile = joinpath(cache_dir, "log.txt");
+
+julia> function f(logfile)
+           mkpath(dirname(logfile))
+           open(logfile, "w") do io
+               print(io, now())
+           end
+       end
+f (generic function with 1 method)
+
+julia> with_cache_dir(cache_dir) do
+    f(logfile)
+    @info "This gets called"
+end
+[ Info: This gets called
+true
+
+julia> with_cache_dir(cache_dir) do
+    f(logfile)
+    @info "This doesn't get called"
+end
+true
+```
+
 """
-function with_cache_dir(f, cache_dir::AbstractString)
-    isdir(cache_dir) && return true
+function with_cache_dir(f, cache_dir::AbstractString; by=isdir)
+    # by(cache_dir) might error if `cache_dir` doesn't exist
+    verify() = isdir(cache_dir) && by(cache_dir)
     clean_up() = rm(cache_dir; force=true, recursive=true)
+
+    verify() && return true
     try
-        f() === true || clean_up()
+        rst = f()
+        if rst === false || !verify()
+            clean_up()
+            return false
+        else
+            return true
+        end
     catch err
         clean_up()
         rethrow(err)
--- test/tst_utils.jl
+++ test/tst_utils.jl
@@ -1,4 +1,4 @@
-using StorageMirrorServer: timeout_call, TimeoutException
+using StorageMirrorServer: timeout_call, TimeoutException, with_cache_dir
 
 @testset "timeout_call" begin
     timeout_err = TimeoutException(1)
@@ -82,5 +82,111 @@ using StorageMirrorServer: timeout_call, TimeoutException
     @test 8_000 < (now() - start_time).value * Threads.nthreads() < 1.5 * 8_000
 end
 
+@testset "with_cache_dir" begin
+    @testset "case 1" begin
+        # case 1: the first function call succeeds
 
+        cache_dir = tempname()
+        logfile = joinpath(cache_dir, "log.txt")
 
+        @test !isdir(cache_dir) && !isfile(logfile)
+        with_cache_dir(cache_dir) do
+            mkpath(cache_dir)
+            open(logfile, "w") do io
+                print(io, "This should be called.")
+            end
+        end
+        @test isdir(cache_dir) && isfile(logfile)
+        isfile(logfile) && @test read(logfile, String) == "This should be called."
+
+        with_cache_dir(cache_dir) do
+            open(logfile, "w") do io
+                print(io, "This doesn't get called")
+            end
+            @test false # mark a fail case if this get called
+        end
+        @test isfile(logfile)
+        isfile(logfile) && @test read(logfile, String) == "This should be called."
+    end
+
+    @testset "case 2" begin
+        # case 2: the first function get called but returns a false
+
+        cache_dir = tempname()
+        logfile = joinpath(cache_dir, "log.txt")
+
+        @test !isdir(cache_dir) && !isfile(logfile)
+        with_cache_dir(cache_dir) do
+            mkpath(cache_dir)
+            open(logfile, "w") do io
+                print(io, "This get called.")
+            end
+            return false
+        end
+        @test !isdir(cache_dir) && !isfile(logfile)
+
+        with_cache_dir(cache_dir) do
+            mkpath(cache_dir)
+            open(logfile, "w") do io
+                print(io, "This get called again.")
+            end
+        end
+        @test isfile(logfile)
+        isfile(logfile) && @test read(logfile, String) == "This get called again."
+    end
+
+    @testset "case 3" begin
+        # case 3: the first get called but an exception is thrown
+
+        cache_dir = tempname()
+        logfile = joinpath(cache_dir, "log.txt")
+
+        @test !isdir(cache_dir) && !isfile(logfile)
+        try
+            with_cache_dir(cache_dir) do
+                mkpath(cache_dir)
+                open(logfile, "w") do io
+                    print(io, "This get called.")
+                end
+                error("some error")
+            end
+        catch err
+            @test err isa ErrorException && err.msg == "some error"
+        end
+        @test !isdir(cache_dir) && !isfile(logfile)
+
+        with_cache_dir(cache_dir) do
+            mkpath(cache_dir)
+            open(logfile, "w") do io
+                print(io, "This get called again.")
+            end
+        end
+        @test isfile(logfile)
+        isfile(logfile) && @test read(logfile, String) == "This get called again."
+    end
+
+    @testset "case 4" begin
+        # case 4: cache isn't valid anymore
+        cache_dir = tempname()
+        logfile = joinpath(cache_dir, "log.txt")
+
+        is_valid_cache(cache_dir) = "log.txt" in readdir(cache_dir)
+
+        @test !isdir(cache_dir)
+        with_cache_dir(cache_dir; by=is_valid_cache) do
+            mkpath(cache_dir)
+            @test !is_valid_cache(cache_dir)
+        end
+        @test !isdir(cache_dir)
+
+        with_cache_dir(cache_dir; by=is_valid_cache) do
+            mkpath(cache_dir)
+            open(logfile, "w") do io
+                print(io, "This get called.")
+            end
+        end
+        @test isfile(logfile)
+        isfile(logfile) && @test read(logfile, String) == "This get called."
+    end
+
+end
